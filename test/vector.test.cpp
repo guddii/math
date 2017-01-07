ec0aea6abd4835572cd4d5365b233a5672e28e4e
Florian Gudat <florian.gudat@guices.com>
2017-01-07 13:00:05 +0100

    Fix vector tests

---
+++ test/vector.test.cpp
@@ -0,0 +1,37 @@
+/**
+ *  @file vector.test.cpp
+ *  @brief Unit test for vector function prototypes.
+ *
+ *  @author Florian Gudat (guddii)
+ *  @see https://github.com/guddii/math
+ *  @bug https://github.com/guddii/math/issues
+ */
+
+#include "../src/vector.hpp"
+#include "gtest/gtest.h"
+
+TEST(vectorTests, atTests) {
+
+    Vector a(0.0, 2.0);
+     EXPECT_EQ(a.at(0), 0);
+     EXPECT_EQ(a.at(1), 2);
+
+    Vector b(0.0, 3.0);
+     EXPECT_EQ(b.at(0), 0);
+     EXPECT_EQ(b.at(1), 3);
+}
+
+TEST(vectorTests, setTests) {
+
+    Vector c(0.0, 2.0);
+    c.set(1, 5.0);
+     EXPECT_EQ(c.at(1), 5);
+
+}
+
+TEST(vectorTests, normTests) {
+
+    Vector d(0.0, 3.0);
+     EXPECT_EQ(d.norm(), 3);
+
+}
